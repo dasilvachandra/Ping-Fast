a883a471a9b5682fcfb22dd5973e63ada47a6a2f
Chandra <chandrad176@gmail.com>
2018-12-12 01:15:51 +0700

    Create Ping.sh

---
+++ Ping.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+s=$1;
+A="$(cut -d'.' -f1 <<<"$s")"
+B="$(cut -d'.' -f2 <<<"$s")"
+C="$(cut -d'.' -f3 <<<"$s")"
+D="$(cut -d'.' -f4 <<<"$s")"
+t=$2
+to=${t:=1}
+
+while [ $D -le $to ]; 
+do 
+	ip="$A.$B.$C.$D";
+	ping -c 1 $ip -w 1 &>/dev/null; 
+	if [ $? -eq 0 ]; then 
+		status="active";
+		echo "Ping $ip $status";
+	else 
+		status="Unreachable"; 
+	fi
+	
+	D=$(( $D + 1 )); 
+done
